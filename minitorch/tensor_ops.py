49503bafeb4cdf0999374d32f35a914612d8abf7
Sylviaviaviavia <103002698+Sylviaviaviavia@users.noreply.github.com>
2024-11-18 12:23:52 -0500

    fix pyright error

--- minitorch/tensor_ops.py
+++ minitorch/tensor_ops.py
@@ -279,8 +279,8 @@ def tensor_map(fn: Callable[[float], float]) -> Any:
         in_strides: Strides,
     ) -> None:
         # TODO: Implement for Task 2.3.
-        out_index: Index = np.zeros(MAX_DIMS, np.int16)
-        in_index: Index = np.zeros(MAX_DIMS, np.int16)
+        out_index: Index = np.zeros(MAX_DIMS, np.int32)
+        in_index: Index = np.zeros(MAX_DIMS, np.int32)
         for i in range(len(out)):
             to_index(i, out_shape, out_index)
             broadcast_index(out_index, out_shape, in_shape, in_index)
